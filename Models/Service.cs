77f6ed1e8003fd394176a2d897dd55080a25ddbf
dnagafonov <denisagafonovwork.gmail.com>
2020-04-16 21:43:28 +0300

    feat: add service route

---
+++ Models/Service.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace coursework_kpiyap.Models
+{
+    public class Service
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string type { get; set; }
+
+        public string name { get; set; }
+
+        public double price { get; set; }
+
+        public string currency { get; set; }
+    }
+}
---
+++ Models/ServiceStoreDatabaseSettings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace coursework_kpiyap.Models
+{
+    public class ServiceStoreDatabaseSettings : IServiceStoreDatabaseSettings
+    {
+        public string ServicesCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+
+    public interface IServiceStoreDatabaseSettings
+    {
+        string ServicesCollectionName { get; set; }
+        string ConnectionString { get; set; }
+        string DatabaseName { get; set; }
+    }
+}
