bbbeee951f4cae23aa948605cb0a3421beb2412c
dnagafonov <denisagafonovwork.gmail.com>
2020-06-07 18:08:48 +0300

    feat: add Offer

---
+++ Services/OfferService.cs
@@ -0,0 +1,35 @@
+﻿using coursework_kpiyap.Models;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace coursework_kpiyap.Services
+{
+    public class OfferService
+    {
+        private readonly IMongoCollection<Offer> _offers;
+
+        public OfferService(IOfferStoreDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+
+            _offers = database.GetCollection<Offer>(settings.OffersCollectionName);
+        }
+
+        public JsonResult Create(Offer offer)
+        {
+            try
+            {
+                _offers.InsertOne(offer);
+                return new JsonResult(new { status = 201 });
+            }
+            catch
+            {
+                return new JsonResult(new { status = 400 });
+            }
+        }
+    }
+}
